b81b3976581f7d61bf8bce93b5769778a9d03404
Netson <netsoncavina@gmail.com>
2022-01-14 20:04:14 -0300

    Added input component

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,3 +1,22 @@
 export default function Meme() {
-  return <h1>Meme</h1>;
+  return (
+    // make two inputs
+    // one for the top text
+    // one for the bottom text
+    // make a button
+    // when the button is clicked,
+    // make a new image with the top text and bottom text
+    // and display it
+    <main>
+      <form className="form">
+        <input className="form--input" type="text" placeholder="Shut up" />
+        <input
+          className="form--input"
+          type="text"
+          placeholder="Take my money"
+        />
+        <button className="form--button">Get a new meme image 🖼</button>
+      </form>
+    </main>
+  );
 }

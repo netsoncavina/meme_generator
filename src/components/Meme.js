2c2960626a4f11433edcd48b6ea4fe00bfdd52f8
Netson <netsoncavina@gmail.com>
2022-01-15 02:40:27 -0300

    Added function to show generated meme on the page

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,22 +1,26 @@
+import memesData from "../memesData";
+import React, { useState } from "react";
+
 export default function Meme() {
+  const [memeImage, setMemeImage] = useState("https://i.imgflip.com/gk5el.jpg");
+  let url;
+  function getMemeImage() {
+    const memesArray = memesData.data.memes;
+    const randomIndex = Math.floor(Math.random() * memesArray.length);
+    url = memesArray[randomIndex].url;
+    setMemeImage(url);
+  }
+
   return (
-    // make two inputs
-    // one for the top text
-    // one for the bottom text
-    // make a button
-    // when the button is clicked,
-    // make a new image with the top text and bottom text
-    // and display it
     <main>
-      <form className="form">
-        <input className="form--input" type="text" placeholder="Shut up" />
-        <input
-          className="form--input"
-          type="text"
-          placeholder="Take my money"
-        />
-        <button className="form--button">Get a new meme image 🖼</button>
-      </form>
+      <div className="form">
+        <input className="form--input" type="text" placeholder="Top text" />
+        <input className="form--input" type="text" placeholder="Bottom Text" />
+        <button onClick={getMemeImage} className="form--button">
+          Get a new meme image 🖼
+        </button>
+      </div>
+      <img className="meme--image" src={memeImage}></img>
     </main>
   );
 }

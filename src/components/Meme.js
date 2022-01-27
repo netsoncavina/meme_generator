b481779c810454d336e0009c6cb5dd895169884a
Netson <netsoncavina@gmail.com>
2022-01-27 15:43:19 -0300

    refactor

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -2,25 +2,38 @@ import memesData from "../memesData";
 import React, { useState } from "react";
 
 export default function Meme() {
-  const [memeImage, setMemeImage] = useState("https://i.imgflip.com/gk5el.jpg");
-  let url;
+  const [meme, setMeme] = useState({
+    topText: "",
+    bottomText: "",
+    randomImg: "https://i.imgflip.com/gk5el.jpg",
+  });
+
+  const [allMemeImage, setAllMemeImage] = useState(memesData);
   function getMemeImage() {
-    const memesArray = memesData.data.memes;
-    const randomIndex = Math.floor(Math.random() * memesArray.length);
-    url = memesArray[randomIndex].url;
-    setMemeImage(url);
+    setMeme({
+      ...meme,
+      randomImg:
+        allMemeImage.data.memes[
+          Math.floor(Math.random() * allMemeImage.data.memes.length)
+        ].url,
+    });
   }
 
   return (
     <main>
       <div className="form">
-        <input className="form--input" type="text" placeholder="Top text" />
+        <input
+          id="top"
+          className="form--input"
+          type="text"
+          placeholder="Top text"
+        />
         <input className="form--input" type="text" placeholder="Bottom Text" />
         <button onClick={getMemeImage} className="form--button">
           Get a new meme image 🖼
         </button>
       </div>
-      <img className="meme--image" src={memeImage}></img>
+      <img className="meme--image" src={meme.randomImg}></img>
     </main>
   );
 }
